73e144e228e11afa937883b15f9954f2b62c41c0
fabrykoppany <koppany.fabry@gmail.com>
2021-05-22 17:42:49 +0300

    Created repository for third project

---
+++ cmake-build-debug/CMakeFiles/project_03.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_03.dir/main.c.obj"
+  "libproject_03.dll.a"
+  "project_03.exe"
+  "project_03.exe.manifest"
+  "project_03.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/project_03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
