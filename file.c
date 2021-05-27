221a98690bc896c4b3f85cc895cf684921a04a68
Daniel <daniel@tohka.us>
2021-05-27 11:35:17 +0300

    Generate combined files

--- file.c
+++ file.c
@@ -34,6 +34,8 @@ void generateStringFile(int n){
         exit(-2);
     }
 
+    fprintf(output_file, "%i\n", n);
+
     for (int i = 0; i < n; ++i){
         int len = rand() % 20 + 1;
 
@@ -57,9 +59,41 @@ void generateIntFile(int n){
         exit(-2);
     }
 
+    fprintf(output_file, "%i\n", n);
+
     for (int i = 0; i < n; ++i){
         int rand_num = rand() % 1000;
         fprintf(output_file, "%i\n", rand_num);
+    }
+
+    fclose(output_file);
+}
+
+void generateCombinedFile(int n){
+    const char *file_name = generateFileName("combined", n);
+    FILE *output_file = fopen(file_name, "w");
+
+    if (output_file == NULL){
+        printf("Error opening %s", file_name);
+        exit(-2);
+    }
+
+    fprintf(output_file, "%i\n", n);
+
+    for (int i = 0; i < n; ++i){
+        if (rand() & 1) {
+            int rand_num = rand() % 1000;
+            fprintf(output_file, "%i\n", rand_num);
+        } else {
+            int len = rand() % 20 + 1;
+
+            for (int j = 0; j < len; ++j){
+                char random_char = 'a' + (char) (rand() % 26);
+                fprintf(output_file, "%c", random_char);
+            }
+
+            fprintf(output_file, "\n");
+        }
     }
 
     fclose(output_file);
@@ -69,5 +103,6 @@ void generateAllFiles(){
     for (int i = 10; i < 1000000; i *= 10){
         generateStringFile(i);
         generateIntFile(i);
+        generateCombinedFile(i);
     }
 }
--- main.c
+++ main.c
@@ -2,11 +2,16 @@
 
 #include "file.h"
 
+#include "time/array_time.h"
+
 int main() {
 
     srand(time(0));
 
     generateAllFiles();
+    worstCaseArray("int", createNumber(5260));
+    bestCaseArray("int", createNumber(597));
+    avgCaseArray("combined");
 
     return 0;
 }
